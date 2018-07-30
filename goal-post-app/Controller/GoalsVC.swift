6b5b77da1312e071a0d789b6db08c371a3a64c50
jem.sai <jem.sai@whitecloak.com>
2018-07-30 11:45:29 +0800

    GoalsVC - setup

--- goal-post-app/Controller/GoalsVC.swift
+++ goal-post-app/Controller/GoalsVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  GoalsVC.swift
 //  goal-post-app
 //
 //  Created by Jemimah Beryl M. Sai on 30/07/2018.
@@ -9,17 +9,17 @@
 import UIKit
 
 class GoalsVC: UIViewController {
-
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func addGoalButtonPressed(_ sender: Any) {
+        
     }
-
-
+    
+    
 }
 
