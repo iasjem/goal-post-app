73aa9cd60f9bbec5c78a778dc36b929441b80811
jem.sai <jem.sai@whitecloak.com>
2018-07-31 17:10:26 +0800

    GoalType - added new file

---
+++ goal-post-app/Enums/GoalType.swift
@@ -0,0 +1,14 @@
+//
+//  GoalType.swift
+//  goal-post-app
+//
+//  Created by Jemimah Beryl M. Sai on 31/07/2018.
+//  Copyright © 2018 Jemimah Beryl M. Sai. All rights reserved.
+//
+
+import Foundation
+
+enum GoalType: String {
+    case longTerm = "Long Term"
+    case shortTerm = "Short Term"
+}
