0e6add4e458fee3761bac27f2474ef681119d251
jem.sai <jem.sai@whitecloak.com>
2018-07-31 17:11:10 +0800

    Button+Ext - added new file

---
+++ goal-post-app/Extensions/Button+Ext.swift
@@ -0,0 +1,19 @@
+//
+//  Button+Ext.swift
+//  goal-post-app
+//
+//  Created by Jemimah Beryl M. Sai on 31/07/2018.
+//  Copyright © 2018 Jemimah Beryl M. Sai. All rights reserved.
+//
+
+import UIKit
+
+extension UIButton {
+    func setSelectedColor() {
+        self.backgroundColor = #colorLiteral(red: 0.4922404289, green: 0.7722371817, blue: 0.4631441236, alpha: 1)
+    }
+    
+    func setDeselectedColor() {
+        self.backgroundColor = #colorLiteral(red: 0.7464011312, green: 0.8857318759, blue: 0.7402122021, alpha: 1)
+    }
+}
