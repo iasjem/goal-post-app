34db87c816401ee773037a28d4bbf76e4b02cb48
jem.sai <jem.sai@whitecloak.com>
2018-07-31 17:31:25 +0800

    GoalsVC - refactored code

--- goal-post-app/Controller/GoalsVC.swift
+++ goal-post-app/Controller/GoalsVC.swift
@@ -9,19 +9,27 @@
 import UIKit
 import CoreData
 
+// MARK: AppDelegate Constant
+
 let appDelegate = UIApplication.shared.delegate as? AppDelegate
 
 class GoalsVC: UIViewController {
     
+    // MARK: Outlets
+    
     @IBOutlet weak var tableView: UITableView!
     
+    // MARK: Initializers
+    
     var goals: [Goal] = []
+    
+    // MARK: View Life Cycles
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
-        tableView.isHidden = true
+        setupView()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -29,10 +37,20 @@ class GoalsVC: UIViewController {
         fetchCoreDataObjects()
     }
     
+    // MARK: Actions
+    
     @IBAction func addGoalButtonPressed(_ sender: Any) {
         guard let createGoalVC = storyboard?.instantiateViewController(withIdentifier: "createGoalVC") else {  return  }
         presentDetail(createGoalVC)
     }
+    
+    // MARK: Setups
+    
+    func setupView() {
+        tableView.isHidden = true
+    }
+    
+    // MARK: Helpers
     
     func fetchCoreDataObjects() {
         self.fetch { (complete) in
@@ -46,53 +64,9 @@ class GoalsVC: UIViewController {
         }
         self.tableView.reloadData()
     }
-}
-
-extension GoalsVC: UITableViewDataSource {
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return goals.count
-    }
+    // MARK: CoreData Functions
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: "goalCell", for: indexPath) as? GoalCell else {   return  UITableViewCell()   }
-        let goal = goals[indexPath.row]
-        cell.configureCell(goal: goal)
-        return cell
-    }
-}
-
-extension GoalsVC: UITableViewDelegate {
-    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    
-    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCellEditingStyle {
-        return .none
-    }
-    
-    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
-        let deleteAction = UITableViewRowAction(style: .destructive, title: "DELETE") { (action, indexPath) in
-            tableView.beginUpdates()
-            self.removeGoal(atIndexPath: indexPath)
-            self.fetchCoreDataObjects()
-            tableView.deleteRows(at: [indexPath], with: .automatic)
-            tableView.endUpdates()
-        }
-        let addAction = UITableViewRowAction(style: .normal, title: "Add 1") { (action, indexPath) in
-            self.setProgress(atIndexPath: indexPath)
-            tableView.reloadRows(at: [indexPath], with: .automatic)
-        }
-        addAction.backgroundColor = #colorLiteral(red: 0.9385011792, green: 0.7164435983, blue: 0.3331357837, alpha: 1)
-        deleteAction.backgroundColor = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
-        return [deleteAction, addAction]
-    }
-}
-
-extension GoalsVC {
     func fetch(completion: @escaping (_ finished: Bool) -> ()) {
         guard let managedContext = appDelegate?.persistentContainer.viewContext else {  return  }
         let fetchRequest = NSFetchRequest<Goal>(entityName: "Goal")
@@ -131,3 +105,50 @@ extension GoalsVC {
     }
 }
 
+// MARK: UITableViewDataSource
+
+extension GoalsVC: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return goals.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "goalCell", for: indexPath) as? GoalCell else {   return  UITableViewCell()   }
+        let goal = goals[indexPath.row]
+        cell.configureCell(goal: goal)
+        return cell
+    }
+}
+
+// MARK: UITableViewDelegate
+
+extension GoalsVC: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCellEditingStyle {
+        return .none
+    }
+    
+    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+        let deleteAction = UITableViewRowAction(style: .destructive, title: "DELETE") { (action, indexPath) in
+            tableView.beginUpdates()
+            self.removeGoal(atIndexPath: indexPath)
+            self.fetchCoreDataObjects()
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+            tableView.endUpdates()
+        }
+        let addAction = UITableViewRowAction(style: .normal, title: "Add 1") { (action, indexPath) in
+            self.setProgress(atIndexPath: indexPath)
+            tableView.reloadRows(at: [indexPath], with: .automatic)
+        }
+        addAction.backgroundColor = #colorLiteral(red: 0.9385011792, green: 0.7164435983, blue: 0.3331357837, alpha: 1)
+        deleteAction.backgroundColor = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
+        return [deleteAction, addAction]
+    }
+}
