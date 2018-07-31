09237c20441635ba977d6f6ca0ac91d9edd1819d
jem.sai <jem.sai@whitecloak.com>
2018-07-31 17:10:03 +0800

    CreateGoalVC - added new file

---
+++ goal-post-app/Controller/CreateGoalVC.swift
@@ -0,0 +1,58 @@
+//
+//  CreateGoalVC.swift
+//  goal-post-app
+//
+//  Created by Jemimah Beryl M. Sai on 31/07/2018.
+//  Copyright © 2018 Jemimah Beryl M. Sai. All rights reserved.
+//
+
+import UIKit
+
+class CreateGoalVC: UIViewController {
+
+    @IBOutlet weak var goalTextView: UITextView!
+    @IBOutlet weak var shortTermButton: UIButton!
+    @IBOutlet weak var longTermButton: UIButton!
+    @IBOutlet weak var nextButton: UIButton!
+    
+    var goalType: GoalType = .shortTerm
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        nextButton.bindToKeyboard()
+        shortTermButton.setSelectedColor()
+        longTermButton.setDeselectedColor()
+        goalTextView.delegate = self
+    }
+    
+    @IBAction func backButtonPressed(_ sender: Any) {
+        dismissDetail()
+    }
+    
+    @IBAction func shortTermButtonSelected(_ sender: Any) {
+        goalType = .shortTerm
+        shortTermButton.setSelectedColor()
+        longTermButton.setDeselectedColor()
+    }
+    
+    @IBAction func longTermButtonSelected(_ sender: Any) {
+        goalType = .longTerm
+        longTermButton.setSelectedColor()
+        shortTermButton.setDeselectedColor()
+    }
+    
+    @IBAction func nextButtonPressed(_ sender: Any) {
+        if goalTextView.text != "" && goalTextView.text != "What is your goal?" {
+            guard let finishGoalVC = storyboard?.instantiateViewController(withIdentifier: "finishGoalVC") as? FinishGoalVC else {  return  }
+            finishGoalVC.initData(description: goalTextView.text!, type: goalType)
+            presentingViewController?.presentSecondaryDetail(finishGoalVC)
+        }
+    }
+}
+
+extension CreateGoalVC: UITextViewDelegate {
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        goalTextView.text = ""
+        goalTextView.textColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+    }
+}
