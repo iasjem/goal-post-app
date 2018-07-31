0bd26efca7608dbbead0f0d1f91d71e126a0f5e2
jem.sai <jem.sai@whitecloak.com>
2018-07-31 11:52:50 +0800

    Project - added new file

---
+++ goal-post-app/View/GoalCell.swift
@@ -0,0 +1,24 @@
+//
+//  GoalCell.swift
+//  goal-post-app
+//
+//  Created by Jemimah Beryl M. Sai on 31/07/2018.
+//  Copyright © 2018 Jemimah Beryl M. Sai. All rights reserved.
+//
+
+import UIKit
+
+class GoalCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
